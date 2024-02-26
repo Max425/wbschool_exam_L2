316ab8db7d9edf6f071b09429950abd3f390c005
Max425 <max.4251474@yandex.ru>
2024-02-26 14:56:51 +0300

    dev 7-8

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -7,32 +13,49 @@ package main
 Одним из вариантов было бы очевидно написать выражение при помощи select, которое бы реализовывало эту связь,
 однако иногда неизестно общее число done каналов, с которыми вы работаете в рантайме.
 В этом случае удобнее использовать вызов единственной функции, которая, приняв на вход один или более or каналов, реализовывала весь функционал.
-
-Определение функции:
-var or func(channels ...<- chan interface{}) <- chan interface{}
-
-Пример использования функции:
-sig := func(after time.Duration) <- chan interface{} {
-	c := make(chan interface{})
-	go func() {
-		defer close(c)
-		time.Sleep(after)
-}()
-return c
-}
-
-start := time.Now()
-<-or (
-	sig(2*time.Hour),
-	sig(5*time.Minute),
-	sig(1*time.Second),
-	sig(1*time.Hour),
-	sig(1*time.Minute),
-)
-
-fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	ctx, cancel := context.WithCancel(context.Background())
+	// Создаем новый канал, который будет использоваться для объединения входных каналов
+	myChan := make(chan interface{})
+	for i, ch := range channels {
+		ch := ch // для go 1.22 эта строчка не нужна)
+		i := i
+		go func() { // Для каждого канала создаем новую горутину
+			select {
+			case <-ch: // Ждем данных из текущего канала
+				fmt.Printf("%d gorutine end work\n", i)
+				close(myChan) // При получении данных закрываем общий канал
+				cancel()
+			case <-ctx.Done():
+				fmt.Printf("%d gorutine close\n", i)
+				break
+			}
+		}()
+	}
+	return myChan
+}
 
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("Done after %v", time.Since(start))
 }
--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,19 +1,70 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
-Необходимо реализовать собственный шелл
+Необходимо реализовать свой собственный UNIX-шелл-утилиту с поддержкой ряда простейших команд:
+- cd <args> - смена директории (в качестве аргумента могут быть то-то и то)
+- pwd - показать путь до текущего каталога
+- echo <args> - вывод аргумента в STDOUT
+- kill <args> - "убить" процесс, переданный в качесте аргумента (пример: такой-то пример)
+- ps - выводит общую информацию по запущенным процессам в формате *такой-то формат*
 
-встроенные команды: cd/pwd/echo/kill/ps
-поддержать fork/exec команды
-конвеер на пайпах
+Так же требуется поддерживать функционал fork/exec-команд
 
-Реализовать утилиту netcat (nc) клиент
-принимать данные из stdin и отправлять в соединение (tcp/udp)
+Дополнительно необходимо поддерживать конвейер на пайпах (linux pipes, пример cmd1 | cmd2 | .... | cmdN).
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
+LOOP:
+	for {
+		dir, _ := os.Getwd()
+		fmt.Printf("%s$ ", dir)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Fprintln(os.Stderr, "Ошибка чтения ввода:", err)
+			continue
+		}
+		input = strings.TrimSpace(input)
+		if input == "" {
+			continue
+		}
+		args := strings.Fields(input)
+		switch args[0] {
+		case "":
+			continue
+		case "exit":
+			break LOOP
+		case "cd":
+			if len(args) < 2 {
+				fmt.Fprintln(os.Stderr, "Неверное количество аргументов для cd")
+				continue
+			}
+			if err = os.Chdir(args[1]); err != nil {
+				fmt.Fprintln(os.Stderr, "Ошибка при смене директории:", err)
+			}
+		default:
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err = cmd.Run(); err != nil {
+				fmt.Fprintln(os.Stderr, "Ошибка при выполнении команды:", err)
+			}
+		}
+	}
 }
